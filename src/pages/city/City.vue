bf519ddf4916374e9f7363b84cc515253405a075
Bian2017 <bgl_cumt_zju@163.com>
2018-11-05 14:37:49 +0800

    feat:添加city路由

---
+++ src/pages/city/City.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <city-header :city="city"></city-header>
+  </div>
+</template>
+
+<script>
+import CityHeader from './components/Header'
+
+export default {
+  name: 'City',
+  data: function () {
+    return {
+      city: ''
+    }
+  },
+  components: {
+    CityHeader
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -27,7 +29,7 @@ export default {
 @import ('~styles/varibles.styl')
 .header
   display: flex
-  line-height: .86rem
+  line-height: $headerHeight
   background: $bglColor
   color: #fff
   .header-left
@@ -47,6 +49,7 @@ export default {
     color: #ccc
   .header-right
     width: 1.24rem
+    color: #fff
     text-align: center
     .arrow-icon
       margin-left: -0.02rem
